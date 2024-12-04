e84eda8cda0db95594835cc1ee62d18b00c74aeb
Lasty <lastycrophopper@gmail.com>
2024-12-04 19:49:05 +0100

    maxwell pumpkin wizard

---
+++ maxwell/maxwell.js
@@ -0,0 +1,13 @@
+
+const title = document.querySelector('.centered-title');
+
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) { 
+        title.style.transform = 'translateY(-100px)'; 
+        title.style.opacity = '0'; 
+    } else { 
+        title.style.transform = 'translateY(0)'; 
+        title.style.opacity = '1'; 
+    }
+});
